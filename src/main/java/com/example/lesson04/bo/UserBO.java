6543e6fcced331b9a5f566d5c3a99cbc8ceb5d55
1J-Choi <chldnjswp110@naver.com>
2024-10-10 21:44:43 +0900

    2024 10 10 수업

--- src/main/java/com/example/lesson04/bo/UserBO.java
+++ src/main/java/com/example/lesson04/bo/UserBO.java
@@ -1,9 +1,9 @@
 package com.example.lesson04.bo;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.lesson04.domain.User;
 import com.example.lesson04.mapper.UserMapper;
 
 @Service
@@ -13,5 +13,9 @@ public class UserBO {
 	
 	public void addUser(String name, String yyyymmdd, String email, String introduce) {
 		userMapper.insertUser(name, yyyymmdd, email, introduce);
+	}
+	
+	public User getLatestUser() {
+		return userMapper.selectLatestUser();
 	}
 }
--- src/main/java/com/example/lesson04/mapper/UserMapper.java
+++ src/main/java/com/example/lesson04/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.example.lesson04.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.example.lesson04.domain.User;
+
 @Mapper
 public interface UserMapper {
 	public void insertUser(
@@ -10,4 +12,7 @@ public interface UserMapper {
 			@Param("yyyymmdd") String yyyymmdd, 
 			@Param("email") String email, 
 			@Param("introduce") String introduce);
+	// input: X
+	// output: User
+	public User selectLatestUser();
 }
